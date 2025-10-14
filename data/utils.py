0c1768cdfe842fd9ae3408319deb7fdfe4a219ce
Purin1410-laptop <ngminhkhoayj2706@gmail.com>
2025-10-14 23:17:46 +0700

    fix for datamodule dont read ' ' in load vocab

--- data/utils.py
+++ data/utils.py
@@ -147,7 +147,7 @@ def extract_data(
     missing = 0
 
     for line in captions:
-        parts = line.strip().split()
+        parts = line.decode().strip().split()
         if len(parts) == 0:
             continue
         img_stem = parts[0]          # không nhất thiết đã có .ext
