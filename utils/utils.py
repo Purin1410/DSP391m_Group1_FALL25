c9f4af2b198fa93db1bb7c6b8c3309a6c6cecc81
Purin1410-laptop <ngminhkhoayj2706@gmail.com>
2025-10-14 16:10:16 +0700

    fix the link dir in utils/utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@ from typing import List, Optional, Tuple, Union
 
 import torch
 import torch.nn.functional as F
-from .datamodule import CROHMEDatamodule
+from data.datamodule import CROHMEDatamodule
 vocab = CROHMEDatamodule.vocab
 
 from einops import rearrange
