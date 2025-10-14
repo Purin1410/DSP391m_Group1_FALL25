969f9e5480b292f09da3d6256256ed5334238107
Purin1410-laptop <ngminhkhoayj2706@gmail.com>
2025-10-14 16:29:54 +0700

    fix error

--- train.py
+++ train.py
@@ -158,7 +158,7 @@ def train(cfg: Config):
     # 5) Trainer
     trainer = pl.Trainer(
         accelerator=cfg.trainer.get("accelerator", "gpu"),
-        devices=cfg.trainer.get("devices", 1),
+        gpus=cfg.trainer.get("devices", 1),
         precision=cfg.trainer.get("precision", 32),
         max_epochs=cfg.trainer.get("max_epochs", 100),
         gradient_clip_val=cfg.trainer.get("gradient_clip_val", 0.0),
