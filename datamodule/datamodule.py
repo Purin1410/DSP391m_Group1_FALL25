072bbf2ea6db73f0356f3f129ff2215f2bb2798e
Purin1410-laptop <ngminhkhoayj2706@gmail.com>
2025-10-15 14:24:42 +0700

    remove x_image

--- datamodule/datamodule.py
+++ datamodule/datamodule.py
@@ -47,7 +47,7 @@ class CROHMEDatamodule(pl.LightningDataModule):
         images_x = batch[1]
         seqs_y = [self.vocab.words2indices(x) for x in batch[2]]
         
-        images_x = [torch.as_tensor(s, dtype=torch.float32).unsqueeze(0) if not torch.is_tensor(s) else s for s in images_x]
+        # images_x = [torch.as_tensor(s, dtype=torch.float32).unsqueeze(0) if not torch.is_tensor(s) else s for s in images_x]
 
         heights_x = [s.size(1) for s in images_x]
         widths_x = [s.size(2) for s in images_x]
